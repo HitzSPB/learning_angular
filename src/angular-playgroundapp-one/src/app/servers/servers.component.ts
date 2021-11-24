5c2215365c7025bd288b546b381d6b2d61b04849
HitzSPB <daniel.oesterbro@gmail.com>
2021-11-24 22:37:38 +0100

    Playing with bindings

--- src/angular-playgroundapp-one/src/app/servers/servers.component.ts
+++ src/angular-playgroundapp-one/src/app/servers/servers.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -7,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'test';
+  constructor() { 
+    setTimeout(() => {this.allowNewServer = true},2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
 }
